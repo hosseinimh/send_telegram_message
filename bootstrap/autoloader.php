6902417b544cb709de72cdac8e18eec5d0ee95f5
Mahmoud Hosseini <hosseinimh@gmail.com>
2023-01-15 11:02:59 +0330

    autoloader.php

--- bootstrap/autoloader.php
+++ bootstrap/autoloader.php
@@ -1,13 +1,10 @@
 <?php
 
-$upDirectory = DIRECTORY_SEPARATOR . '..';
-$path = __DIR__ . $upDirectory . DIRECTORY_SEPARATOR;
-
 require_once(__DIR__ . '/../config.php');
-require_once $path . 'app/helpers.php';
+require_once ABS_PATH . '/app/helpers.php';
 
-spl_autoload_register(function ($class) use ($path) {
-    $file = $path . lcfirst(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
+spl_autoload_register(function ($class) {
+    $file = ABS_PATH . DIRECTORY_SEPARATOR . lcfirst(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
 
     if (file_exists($file)) {
         require_once $file;
